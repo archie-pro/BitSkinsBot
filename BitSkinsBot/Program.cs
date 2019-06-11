be2060f9783227c4eeb80e21763ba0f8df65e865
Artsiom Prakapovich <51159427+archie-pro@users.noreply.github.com>
2019-06-11 18:15:21 +0300

    minor changes

--- BitSkinsBot/Program.cs
+++ BitSkinsBot/Program.cs
@@ -10,12 +10,6 @@ namespace BitSkinsBot
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
-
-			BotConfigurator.Configure("0b4997a8-00cb-428c-a245-7900dfdf872e", "27DBI2H24AYKNKEN");
-
-			var service = BotConfigurator.Container.Resolve<AccountService>();
-
-			Console.WriteLine(service.GetAccountBalance().AvailiableBalance);
 		}
 	}
 }
